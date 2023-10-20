2bd168b09439e4c5e483bcc126b509c1f9cc48bc
mmwandera <mmwandera74@gmail.com>
2023-10-20 13:22:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const palindrome = word.split("").reverse().join("")
+
+  if(word === palindrome){
+    return true;
+  } else{
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+
+  input = String
+  output = Boolean
+
+  Function takes in a string
+  Uses if function to check whether its a paindrome or not
+  returns true if its a palindrome and false if not
 */
 
 /*
   Add written explanation of your solution here
+
+  case 1:
+  word = racecar
+  function checks if racecar === racecar(racecar spelled backwards)
+  returns true if so
+  i.e isPalindrome("racecar") => true
+
+  case 2:
+  word = conclusion
+  Is conclusion === noisulcnoc(conclusion spelled backwards)
+  returns false
+  isPalindrome("conclusion") => false
 */
 
 // You can run `node index.js` to view these console logs
